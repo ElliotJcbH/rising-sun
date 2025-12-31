9486baff215e177092a6e9a71f508b4aaefaebc0
Elliot Halili <elliot.jacob09@gmail.com>
2025-12-31 11:15:59 +0800

    added local network listener to server

--- web-client/src/pages/__root.tsx
+++ web-client/src/pages/__root.tsx
@@ -1,18 +1,33 @@
 import { Outlet } from "@tanstack/react-router";
 import '@/styles/global.css';
 import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 const Root = () => {
 
-    const [authToken, setAuthToken] = useState(window.localStorage.getItem("authToken"));
+    const [authToken, setAuthToken] = useState<string | null>(window.localStorage.getItem("authToken") || '');
 
     useEffect(() => {
-        if(!authToken) return;
+        if(!authToken) {
+            clearAuthToken();
+            return;
+        };
         
         const accessToken = JSON.parse(authToken).accessToken;
-        if(!accessToken) return;
-        
-        setInterval
+        if(!accessToken){
+            clearAuthToken();
+            return;
+        }
+
+        const decodedToken = jwtDecode(accessToken);
+        const remainingTime = Date.now() - (decodedToken.exp || Infinity);
+
+        if(remainingTime <= 0 || remainingTime == Infinity) {
+            clearAuthToken();
+            return;
+        };
+
+        setTimeout(() => {clearAuthToken()}, remainingTime)
     }, [authToken]);
 
     window.addEventListener('storage', (e) => {
@@ -21,10 +36,27 @@ const Root = () => {
         }
     })
 
+    async function clearAuthToken() {
+        window.localStorage.removeItem("authToken");
+        setAuthToken('');
+
+        const newAccessToken = await fetch(`${import.meta.env.VITE_API_URL}/refresh-access-token`, {
+            method: "GET",
+                 headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: data.username,
+                email: data.email,
+                password: data.password
+            })
+        })
+    }
+
     return (
         <div>
             <h1>Fucker</h1>
-            <Outlet />
+            <Outlet  />
         </div>
     )
 
